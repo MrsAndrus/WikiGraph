0adf1590845dedcf65cbb8b0266458d48f4ecd90
Andrushchenko, Oleksandr <andr2000@gmail.com>
2016-12-10 22:27:38 +0200

    Basic page parsing
    Signed-off-by: Andrushchenko, Oleksandr <andr2000@gmail.com>

--- src/graph/Main.java
+++ src/graph/Main.java
@@ -2,8 +2,8 @@ package graph;
 
 public class Main {
 
-	private static String DEFAULT_URL = "https://en.wikipedia.org/wiki/";
-	private static String GRAF_VERTEX = "Scientometrics";
+	private static String DEFAULT_URL = "https://en.wikipedia.org";
+	private static String GRAF_VERTEX = "/wiki/Scientometrics";
 
 	public static void main(String[] args) {
 		new Root(DEFAULT_URL, GRAF_VERTEX).run();
--- src/graph/Root.java
+++ src/graph/Root.java
@@ -1,7 +1,13 @@
 package graph;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 public class Root {
 
@@ -16,7 +22,33 @@ public class Root {
 	}
 
 	public void run() {
-		System.out.print("Using " + mPath + "/" + mGrafVertex + " as root URL");
-//		Document doc = Jsoup.connect("http://daniweb.com/").get();
+		System.out.println("Using " + mPath + "/" + mGrafVertex + " as root URL");
+		try {
+			Document doc = Jsoup.connect(mPath + "/" + mGrafVertex).get();
+
+			Elements elements = doc.body().select("p");
+			for (Element p : elements) {
+				System.out.println("---------------------------------------");
+				while (p.tagName().equals("p")) {
+					Elements hrefs = p.select("a");
+
+					for (Element a : hrefs) {
+						System.out.println(a.toString());
+						System.out.println("Title: \"" + a.attr("title") + "\"");
+						System.out.println("href: " + a.attr("href"));
+					}
+					/*
+					for (Element link : hrefs) {
+						System.out.println(link.attr("href"));
+					}
+					*/
+					p = p.nextElementSibling();
+				}
+			}
+		} catch (IOException e) {
+			System.out.print("Failed to retrieve " + mPath + "/" + mGrafVertex
+					+ ": " + e.getMessage());
+			e.printStackTrace();
+		}
 	}
 }
