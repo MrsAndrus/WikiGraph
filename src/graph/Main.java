1a6bcf4317b0afa9fd739dc044419f6291cce568
Oleksandr Andrushchenko <andr2000@gmail.com>
2017-07-18 11:15:13 +0300

    Remove unneeded slash in the vertex path
    Signed-off-by: Oleksandr Andrushchenko <andr2000@gmail.com>

--- src/graph/Main.java
+++ src/graph/Main.java
@@ -3,7 +3,7 @@ package graph;
 public class Main {
 
 	private static String DEFAULT_URL = "https://en.wikipedia.org";
-	private static String GRAF_VERTEX = "/wiki/Scientometrics";
+	private static String GRAF_VERTEX = "wiki/Scientometrics";
 
 	public static void main(String[] args) {
 		new Root(DEFAULT_URL, GRAF_VERTEX).run();
