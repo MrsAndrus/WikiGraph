f46a9d72e4880c517b8b9fd477941011884978f1
Valentyna Andrushchenko <valentyna.andrushchenko@gmail.com>
2017-07-20 12:26:00 +0300

    Add command line parameters
    Signed-off-by: Valentyna Andrushchenko <valentyna.andrushchenko@gmail.com>

--- src/graph/Main.java
+++ src/graph/Main.java
@@ -6,7 +6,20 @@ public class Main {
 	private static String NOTION_DEFAULT = "Scientometrics";
 
 	public static void main(String[] args) {
-		new GraphVertex(WIKI_DEFAULT_URL, NOTION_DEFAULT,
-				GRAF_VERTEX_DEFAULT).run();
+		String wiki_url = WIKI_DEFAULT_URL;
+		String graf_vertex = GRAF_VERTEX_DEFAULT;
+		String notion = NOTION_DEFAULT;
+
+		if (args.length > 1) {
+			graf_vertex = args[0];
+			if (!graf_vertex.startsWith("/wiki")) {
+				graf_vertex = "/wiki/" + graf_vertex;
+			}
+			notion = args[1];
+		}
+		if (args.length > 2) {
+			wiki_url = args[2];
+		}
+		new GraphVertex(wiki_url, notion, graf_vertex).run();
 	}
 }
